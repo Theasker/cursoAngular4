db0884a5351536b0ff1f09893ad743cbef638deb
Mauricio Segura (Theasker) <theasker@gmail.com>
2017-04-20 18:52:58 +0200

    routing

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'contacto',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 
 export class ContactoComponent{
     public titulo = 'Página de contacto de la web';
+    public parametro;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router
+    ){}
+
+    ngOnInit(){
+        this._route.params.forEach((params: Params )=>{
+            console.log(params);
+            this.parametro = params['page'];
+        })
+    }
+
+    redirigir(){
+        this._router.navigate(['/contacto','parametro2']);
+    }
+
+    redirigirHome(){
+        this._router.navigate(['/home']);
+    }
 }
